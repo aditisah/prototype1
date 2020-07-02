079596e5ae3551fc1b13454685f6623cf8f1b4ac
Aditi <sditi.sah1@gmail.com>
2020-07-03 00:36:11 +0530

    kk

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from ''
+import { from } from 'rxjs';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppModule { }
