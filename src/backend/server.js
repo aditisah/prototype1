94b69102717c8428cf566a0af75243903d9aa5db
Aditi <sditi.sah1@gmail.com>
2020-08-12 23:09:04 +0530

    change

--- src/backend/server.js
+++
@@ -1,7 +0,0 @@
-//START SERVER
-const app = require('./app');
-//app.use(express.static(__dirname +'/dist'));
-const port = process.env.port || 3000;
-app.listen(port,()=>{
-  console.log("server is listening....");
-})
