e8c365bf879817d8992e45fd427c9aad3758da72
Aditi <sditi.sah1@gmail.com>
2020-08-12 22:38:48 +0530

    changes done

---
+++ server.js
@@ -0,0 +1,11 @@
+//START SERVER
+const app = require('./src/backend/app');
+const path = require('path');
+app.use(express.static(__dirname +'/dist'));
+const port = process.env.port || 3000;
+app.listen(port,()=>{
+  console.log("server is listening....");
+})
+app.get('/*',(req,res)=>{
+  res.sendFile(path.join(__dirname + '/dist/polytechnic-web/index.html'));
+})
