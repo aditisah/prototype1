89bf6a79af2f12f21f899ced6a3b1f03bbbef2de
Aditi <sditi.sah1@gmail.com>
2020-08-12 10:45:21 +0530

    changes done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //START SERVER
 const app = require('./app');
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + 'dist'));
 const port = process.env.port || 3000;
 app.listen(port,()=>{
   console.log("server is listening....");
