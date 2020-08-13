4139db714fe9f8dcfc4d90fe44ab5eece292e8bb
Aditi <sditi.sah1@gmail.com>
2020-08-13 10:41:03 +0530

    change

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const port = process.env.port || 3000;
 app.listen(port,()=>{
   console.log("server is listening....");
 })
-app.get('/*',(req,res)=>{
-  res.sendFile(path.join(__dirname + '/dist/polytechnic-web/index.html'));
-})
+// app.get('/*',(req,res)=>{
+//   res.sendFile(path.join(__dirname + '/dist/polytechnic-web/index.html'));
+// })
