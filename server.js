a4575f5734cfb0dc7e6fed780600b46ca1f500ec
Aditi <sditi.sah1@gmail.com>
2020-08-14 00:23:17 +0530

    s

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //START SERVER
 const app = require('./src/backend/app');
-app.use(express.static(__dirname +'/dist/polytechnic-web'));
+app.use(express.static('./dist/polytechnic-web'));
 const port = process.env.port || 3000;
 app.listen(port,()=>{
   console.log("server is listening....");
