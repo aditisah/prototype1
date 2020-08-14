666daab07a0a390a2e9f49df67368a37b4a1f1b0
Aditi <sditi.sah1@gmail.com>
2020-08-14 10:54:30 +0530

    change in app path

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //START SERVER
-const app = require('./../polytechnic-web/src/backend/app');
+const app = require('/app');
 app.use(express.static(_dir +'./dist/polytechnic-web'));
 const port = process.env.port || 3000;
 app.listen(port,()=>{
