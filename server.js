2038da9d4b183dc586bfdb804eb83c10291040dc
Aditi <sditi.sah1@gmail.com>
2020-08-16 01:14:56 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 //START SERVER
 const app = require('./app');
-//app.use(express.static(_dir +'./dist/polytechnic-web'));
+app.use(express.static(_dir +'./dist/polytechnic-web'));
 const port = process.env.port || 3000;
 app.listen(port,()=>{
   console.log("server is listening....");
 })
-// app.get('/*', function (req, sres) {
-//   res.sendFile('index.html', { root: 'dist/polytechnic-web' }
-//   );
-// });
+ app.get('/*', function (req, sres) {
+   res.sendFile('index.html', { root: 'dist/polytechnic-web' }
+   );
+ });
 
