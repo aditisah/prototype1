96f4262bbc85f9a2c20ce80f07603ab1b088f05f
Aditi <sditi.sah1@gmail.com>
2020-08-21 09:44:16 +0530

    change in url

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 //const path = require('path');
 const app = require('./app');
 //app.use(express.static(_dir +'./dist/polytechnic-web'));
-const {port = 3000,LOCAL_ADDRESS='0.0.0.0/0'} = process.env
-app.listen((port,LOCAL_ADDRESS),()=>{
+const port = process.env.PORT||3000;
+app.listen(port,()=>{
   console.log("server is listening....");
 })
   app.get('/*', (req, sres)=>{
