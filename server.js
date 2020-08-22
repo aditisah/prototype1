2e664535a97d53ec83459d8f5d3c81c1faf8c305
Aditi <sditi.sah1@gmail.com>
2020-08-22 13:29:03 +0530

    delete app.get line

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT||3000;
 app.listen(port,()=>{
   console.log("server is listening....");
 })
-  app.get('/*', (req, sres)=>{
-    res.sendFile('index.html', { root: 'dist/polytechnic-web' }
-    );
-  });
+  // app.get('/*', (req, sres)=>{
+  //   res.sendFile('index.html', { root: 'dist/polytechnic-web' }
+  //   );
+ // });
 
